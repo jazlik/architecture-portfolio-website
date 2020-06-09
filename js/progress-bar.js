27213c2cd4793dd56d36bc4a316db3a7d3790700
jazlik <jakubkozlik5@gmail.com>
2020-06-09 23:04:00 +0200

    Poprawione progress bary

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -1,16 +1,11 @@
 const progresses = document.querySelectorAll('.progress-done');
 
 progresses.forEach(progress => {
-    const done = +progress.getAttribute('data-done');
-    const duration = getComputedStyle(progress).transitionDuration;
-    const timeoutDuration = +duration.slice(0, 1) * 1000;
-
-    progress.style.width = done + '%';
+    progress.style.width = progress.getAttribute('data-done') + '%';
     progress.style.opacity = 1;
-
-    for (let i = 0; i <= done; i++) {
-        setTimeout(() => {
-            progress.innerText = i + '%';
-        }, (timeoutDuration / done) * i);
-    }
 });
+
+setTimeout(() => {
+    const progressTexts = document.querySelectorAll('.progress-text');
+    progressTexts.forEach(t =>  t.style.color = 'white' );
+}, 2000)
