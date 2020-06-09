4b7a7f37eb42411d4bc7b0aaea8562ed52100e64
arek <arek.mikolajczak19@gmail.com>
2020-06-09 22:26:42 +0100

    Constant speed of progress bars and dynamic text animation

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -9,12 +9,15 @@ function expandProgressBars() {
     const progresses = document.querySelectorAll('.progress-done');
 
     progresses.forEach(progress => {
+        const width = +progress.getAttribute('data-done');
+        const duration = width/100 * 5;
+        progress.style.transition = `all ${duration}s ease 0.3s`;
         progress.style.width = progress.getAttribute('data-done') + '%';
         progress.style.opacity = 1;
-    });
 
-    setTimeout(() => {
-        const progressTexts = document.querySelectorAll('.progress-text');
-        progressTexts.forEach(t =>  t.style.color = 'white' );
-    }, 2000)
+      setTimeout(() => {
+        const progressText = progress.parentNode.querySelectorAll('.progress-text')[0];
+        progressText.style.color = 'white';
+      }, (duration < 0.5 ? 0 : duration - 0.5) * 1000)
+    });
 }
